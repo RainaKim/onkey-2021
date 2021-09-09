2b2014c9a49a43b9afaa8e8c4830b85f13188b79
Raina Kim <hrk296@nyu.edu>
2021-09-09 17:19:58 +0900

    fixed precompile)

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Onkey2021
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.precompile += %w( print.css )
   end
 end
